7a70a5817f111981e9255606d9466df4c06057bd
ryananger <nightcrawling@gmail.com>
2023-08-17 12:55:54 -0400

    ...

--- client/src/components/UI/Loading.jsx
+++ client/src/components/UI/Loading.jsx
@@ -1,11 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Loading = function() {
   const [style, setStyle] = useState({opacity: 1});
 
-  setTimeout(function() {
-    setStyle({opacity: 0, zIndex: '-10'});
-  }, 2000);
+  useEffect(()=>{
+    setTimeout(function() {
+      setStyle({opacity: 0});
+
+      setTimeout(()=>{setStyle({opacity: 0, zIndex: '-10'})}, 1000);
+    }, 2000);
+  }, []);
 
   return (
     <div className='loading float v' style={style}>
